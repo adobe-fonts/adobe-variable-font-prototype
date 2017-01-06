992b299cb9239d4622bab2ad21b1b064e0bc80bb
Miguel Sousa <msousa@adobe.com>
2017-01-06 11:46:27 -0800

    Subset font made for the Unicode text rendering tests repo (https://github.com/unicode-org/text-rendering-tests). This font only supports the dollar $ character.

--- buildFont.sh
+++ buildFont.sh
@@ -1,3 +1,3 @@
 buildMasterOTFs RomanMasters/AdobeVFPrototype.designspace
-buildCFF2VF RomanMasters/AdobeVFPrototype.designspace  AdobeVFPrototype.otf
-ttx -o AdobeVFPrototype.otf -m AdobeVFPrototype.otf name_GSUB_changes.ttx
+buildCFF2VF RomanMasters/AdobeVFPrototype.designspace  AdobeVFPrototype-Subset.otf
+ttx -o AdobeVFPrototype-Subset.otf -m AdobeVFPrototype-Subset.otf name_GSUB_changes.ttx
