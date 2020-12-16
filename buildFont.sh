ef987a1e8f9c4bac9ed30256b78b99785a726183
Josh Hadley <johadley@adobe.com>
2020-12-16 10:31:09 -0800

    Update buildFont.sh
    fiddle with STAT.fea hack so we don't leave the file modified if there's a failure in `fontmake`

--- buildFont.sh
+++ buildFont.sh
@@ -25,11 +25,28 @@ tx -cff2 +S +b -std "$otf_file" "$folder/.tb_cff2" 2> /dev/null
 # replace CFF2 table with subroutinized version
 sfntedit -a CFF2="$folder/.tb_cff2" "$otf_file" 1> /dev/null
 
+# TEMP HACK until fontTools.feaLib supports STAT
 # comment out STAT feature file which cannot be digested by fontmake
 sed -i '' 's/^/#/' $folder/STAT.fea
 
-# build the TTF version
+# set +e so we have the opportunity to un-hack STAT.fea before exiting if
+# there's a problem with fontmake.
+set +  # 
+# build TTF with fontmake
 fontmake -m "$dsp_file" -o variable --production-names --output-path "$ttf_file"
+fm_status=$?
+
+# undo changes to STAT feature file
+sed -i '' 's/#//' $folder/STAT.fea
+
+if [ $fm_status -ne 0 ]
+then
+    exit $fm_status
+fi
+
+# return to exit-on-fail mode
+set -e
+
 
 # use DSIG, name, OS/2, hhea, post, and STAT tables from OTF
 sfntedit -x DSIG="$folder/.tb_DSIG",name="$folder/.tb_name",OS/2="$folder/.tb_os2",hhea="$folder/.tb_hhea",post="$folder/.tb_post",STAT="$folder/.tb_STAT" "$otf_file" 1> /dev/null
@@ -42,8 +59,5 @@ sfntedit -a cmap="$folder/.tb_cmap",GDEF="$folder/.tb_GDEF",GPOS="$folder/.tb_GP
 # delete temporary files
 rm "$folder"/.tb_*
 rm "$folder"/master_*/*.otf
-
-# undo changes to STAT feature file
-sed -i '' 's/#//' $folder/STAT.fea
 
 echo "Done"
